d4aa3626c288cc2469247a93cd3a76a4f1f1c1d1
littlethe <littlethe2000@yahoo.com.tw>
2020-03-14 18:54:16 +0000

    'fff'

---
+++ Notification/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Notification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxTitle = new System.Windows.Forms.TextBox();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelResponse = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxTopic = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(295, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 52);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxTitle
+            // 
+            this.textBoxTitle.Location = new System.Drawing.Point(295, 113);
+            this.textBoxTitle.Name = "textBoxTitle";
+            this.textBoxTitle.Size = new System.Drawing.Size(176, 25);
+            this.textBoxTitle.TabIndex = 2;
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Location = new System.Drawing.Point(295, 144);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(176, 89);
+            this.textBoxBody.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Body:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(210, 331);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Response:";
+            // 
+            // labelResponse
+            // 
+            this.labelResponse.AutoSize = true;
+            this.labelResponse.ForeColor = System.Drawing.Color.Red;
+            this.labelResponse.Location = new System.Drawing.Point(292, 331);
+            this.labelResponse.Name = "labelResponse";
+            this.labelResponse.Size = new System.Drawing.Size(39, 15);
+            this.labelResponse.TabIndex = 7;
+            this.labelResponse.Text = "None";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Topic:";
+            // 
+            // textBoxTopic
+            // 
+            this.textBoxTopic.Location = new System.Drawing.Point(295, 82);
+            this.textBoxTopic.Name = "textBoxTopic";
+            this.textBoxTopic.Size = new System.Drawing.Size(176, 25);
+            this.textBoxTopic.TabIndex = 9;
+            this.textBoxTopic.Text = "notification";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxTopic);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelResponse);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxBody);
+            this.Controls.Add(this.textBoxTitle);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Group15 ASE Notification Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxTitle;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelResponse;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxTopic;
+    }
+}
+
---
+++ Notification/Form1.cs
@@ -0,0 +1,73 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace Notification
+{
+    public partial class Form1 : Form
+    {
+        public FirebaseMessaging messaging;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string SERVER_API_KEY = "AAAA9_hwQQs:APA91bHb-3C4gpIGK999bZTBl93xYf-pXl7M6gJTRROhmyahqKkgjdCZpUNqrl2EfjJkW0DXS2AZHZgy_2gu6oXyOSRM-IDw6rHZXnSkdqkxopJjS4hgZabJxaywpOHYOTrCxY_u6iZC";
+            var SENDER_ID = "1065025028363";
+
+            WebRequest tRequest;
+            tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+
+            tRequest.Method = "post";
+            tRequest.ContentType = "application/json";
+            tRequest.Headers.Add(string.Format("Authorization: key={0}", SERVER_API_KEY));
+            tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
+
+
+            var data = new
+            {
+                notification = new
+                {
+                    title = textBoxTitle.Text,
+                    body = textBoxBody.Text,
+                }
+                  ,
+                to = "/topics/" + textBoxTopic.Text
+            };
+
+            var serializer = new JavaScriptSerializer();
+            var json = serializer.Serialize(data);
+            Byte[] byteArray = Encoding.UTF8.GetBytes(json);
+            tRequest.ContentLength = byteArray.Length;
+
+            Stream dataStream = tRequest.GetRequestStream();
+            dataStream.Write(byteArray, 0, byteArray.Length);
+            dataStream.Close();
+
+            WebResponse tResponse = tRequest.GetResponse();
+            dataStream = tResponse.GetResponseStream();
+            StreamReader tReader = new StreamReader(dataStream);
+            labelResponse.Text = tReader.ReadToEnd();
+            tReader.Close();
+            dataStream.Close();
+            tResponse.Close();
+        }
+
+    }
+}
